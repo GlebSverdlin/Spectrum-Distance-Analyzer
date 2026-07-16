3a6df912b29c4d2f51ce2769ec2947944cf0f664
Gleb Sverdlin <gleb.sverdlin@gmail.com>
2026-07-16 10:12:05 +0200

    started spectrum distance implementation

--- n_dim_space_demo.py
+++ n_dim_space_demo.py
@@ -14,6 +14,28 @@ a_ori = {"Ha": 56, "Hb": 43, "O3": 21, "N2": 36}
 b_ori = {"Ha": 61, "Hb": 36, "O3": 27, "N2": 47}
 t_ori = {"Ha": 32, "Hb": 33, "O3": 42, "N2": 49}
 
+class spec_distance:
+    int dist
+    def __init__(self, from_obj, to_obj): #и from и to - списки величин потоков для соотв. звезд
+        self.spec_from = from_obj
+        self.spec_to = to_obj
+        dist = get_spec_distance(spec_from, spec_to)
+
+    def get_spec_distance(spec_from, spec_to):
+        diffs = list_diff(spec_from, spec_to, True)
+        i = 0
+        while i<len(diffs):
+            a = diffs[i]
+            a = a*a
+            diffs[i]=a
+            i+=1
+        b=0
+        for i in diffs:
+            b+=i
+        dist=math.sqrt(b)
+        return dist
+
+
 def get_spec_vector(list):
     vector = []
     for i in list:
@@ -35,10 +57,6 @@ def list_diff(list_1, list_2, absolute):
     return diff_list
         
 
-a_ori_spec=get_spec_vector(a_ori)
-b_ori_spec=get_spec_vector(b_ori)
-t_ori_spec=get_spec_vector(t_ori)
-
 def get_spec_distance(spec_1, spec_2):
     diffs = list_diff(spec_1, spec_2, True)
     i = 0
@@ -52,6 +70,7 @@ def get_spec_distance(spec_1, spec_2):
         b+=i
     b=math.sqrt(b)
     return b
+'''
 
 fig, ax = plt.subplots()
 
@@ -82,3 +101,8 @@ for i in range(len(stars)):
 #fig.savefig("figure.pdf")
 plt.show()
 print(ab, at, bt)
+
+a_ori_spec=get_spec_vector(a_ori)
+b_ori_spec=get_spec_vector(b_ori)
+t_ori_spec=get_spec_vector(t_ori)
+
