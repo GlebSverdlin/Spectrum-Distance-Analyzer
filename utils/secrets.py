addc1c0350b7be395a774f80437450cef1512acd
Gleb Sverdlin <gleb.sverdlin@gmail.com>
2026-08-10 14:07:43 +0200

    pycache update

--- utils/secrets.py
+++
@@ -1,8 +0,0 @@
-k2_table = "/home/gleb/Astronomy/K2_planet_list.csv"
-kepler_table = "/home/gleb/Astronomy/Kepler_planet_list.csv"
-k2_neg = '/home/gleb/Astronomy/mast_download/k2_neg'
-k2_pos = '/home/gleb/Astronomy/mast_download/k2_pos'
-kep_neg = '/home/gleb/Astronomy/mast_download/kep_neg'
-kep_pos = '/home/gleb/Astronomy/mast_download/kep_pos'
-dataset = '/home/gleb/Programming/Python/SpectrumAnalyzer/dataset/'
-
