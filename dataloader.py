14e6a1e0d22948371c312dd8791762acdd5f296f
Gleb Sverdlin <gleb.sverdlin@gmail.com>
2026-08-14 14:23:13 +0200

    Minor debugging of the dataset class

--- dataloader.py
+++ dataloader.py
@@ -1,22 +1,26 @@
 import os
 import pandas as pd
 from secret import *
+from torch.utils.data import Dataset, DataLoader
 
 class SpectralDataset(Dataset):
     def __init__(self, datatype):
         match datatype:
-            case 'ap': data_dir = dataset_ap
-            case 'aspcap': data_dir = dataset_aspcap
+            case 'ap': self.data_dir = dataset_ap
+            case 'aspcap': self.data_dir = dataset_aspcap
         self.spectra = []
-        for i in os.scandir(data_dir): spectra.append(i.name)
+        for i in os.scandir(self.data_dir): self.spectra.append(i.name)
 
     def __len__(self):
-        return len(spectra)
+        return len(self.spectra)
         
     def __getitem__(self, idx):
-        filename = spectra[idx]
-        path = str(data_dir+filename)
-        data = pd.read_csv(path, usecols='flux')
-        label = pd.read_csv(path, usecols='planet')
+        filename = self.spectra[idx]
+        path = str(self.data_dir+filename)
+        data = pd.read_csv(path, usecols=['flux'], index_col=False)
+        label = pd.read_csv(path, usecols=['planet'], index_col=False)
+        label = label.head(1)
+        data = data.to_numpy(dtype=float, na_value = 0.0)
+        label = label.to_numpy(dtype=float)
         return data, label
 
